bbf51d64d6f9c2fdec344f5798d56c3ea1be487a
Ahmad Rasekh <ahmad.rasekh@moixa.com>
2020-12-03 19:16:11 +0000

    prettier

--- d1/solution.ml
+++ d1/solution.ml
@@ -1,34 +1,41 @@
 #use "../lib/utils.ml"
 
-let input = "input" |> read_file |> List.map int_of_string
+let parse_input input = 
+  input |> 
+  read_file |> 
+  List.map int_of_string
 
-let find_solution ~sum:total ~input:input ~sum_of:sm= 
-  let h = Hashtbl.create (List.length input) in
+  let solve ~sum_of:sm ~sum:total ~input:input_list = 
+    let h = Hashtbl.create (List.length input_list) in
 
   let rec sum_of_two sum = function 
     | hd::tl ->  let need = sum-hd in 
-                  if Hashtbl.mem h (need) then
-                    [hd; need]
-                  else begin
-                    Hashtbl.add h hd hd;
-                    sum_of_two sum tl
-                  end
+      if Hashtbl.mem h (need) then
+        [hd; need]
+      else begin
+        Hashtbl.add h hd hd;
+        sum_of_two sum tl
+      end
     | [] -> [] in
 
   let rec sum_of_three sum = function
     | hd::tl -> (
-                  match sum_of_two (sum-hd) tl with
-                    | [] -> sum_of_three sum tl
-                    | _ as sum_of_two_solution -> hd::sum_of_two_solution
-                )
+        match sum_of_two (sum-hd) tl with
+        | [] -> sum_of_three sum tl
+        | _ as sum_of_two_solution -> hd::sum_of_two_solution
+      )
     | [] -> [] in
 
-    let solution = match sm with 
-      | 2 -> sum_of_two total input
-      | 3 -> sum_of_three total input
-      | _ -> [] in
-    
-    (* assert ((List.fold_left (+) 0 solution) = total); *)
-    
-    solution
+  match sm with 
+    | 2 -> sum_of_two total input_list
+    | 3 -> sum_of_three total input_list
+    | _ -> []
+
+  (* assert ((List.fold_left (+) 0 solution) = total); *)
+
+
+  let _ = 
+    "input" |> 
+    parse_input |> 
+    (fun ls -> solve ~sum_of:3 ~sum:2020 ~input:ls) 
 
