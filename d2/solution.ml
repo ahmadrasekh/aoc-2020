7414746e20f88a7d2425bc305cf55a90c97f4a60
Ahmad Rasekh <ahmad.rasekh@moixa.com>
2020-12-03 19:17:16 +0000

    solutions to day 2

---
+++ d2/solution.ml
@@ -0,0 +1,27 @@
+#use "../lib/utils.ml"
+
+(* TODO: parse "1-3 a: abcde" to (1,3), 'a', "abcde" *)
+let parse_line str = 
+  let f min max ch str = ((min, max), ch, str) in
+  Scanf.sscanf str "%i-%i %c: %s" f
+
+let parse_input input = 
+  input |>
+  read_file |>
+  List.map parse_line
+
+let validate_a (range, ch, str) =  in_range range (count_occurance ch str)
+
+let validate_b ((i, j), ch, str) = 
+  let char_sq = str |> String.to_seq |> List.of_seq in
+  bool_of_int (
+    (lxor)
+    (int_of_bool ((List.nth char_sq (i-1)) = ch))
+    (int_of_bool ((List.nth char_sq (j-1)) = ch))
+  )
+
+let _ = 
+  "input" |>
+  parse_input |>
+  (List.filter validate_b) |>
+  List.length
