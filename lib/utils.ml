e2bf302530436905a5d082aa5be7226cc286106c
Ahmad Rasekh <ahmad.rasekh@moixa.com>
2020-12-03 19:16:50 +0000

    some utility functions

--- lib/utils.ml
+++ lib/utils.ml
@@ -15,7 +15,27 @@ let strip_last_char  = function
   | "" -> ""
   | _ as str -> String.sub str 0 ((String.length str) - 1)
 
-(* ints  *)
+let count_occurance ch str = 
+    let char_seq = String.to_seq str in
+    Seq.fold_left (fun acc c -> if c=ch then acc+1 else acc) 0 char_seq
+    (* let rec aux acc = function
+      | "" -> acc
+      | hd::tl ->  *)
+
+(* ints *)
 let in_range (min, max) x =
-  if min < max then min < x && x < max
+  if min < max then min <= x && x <= max
   else failwith "range format: (min, max) with min < max"
+
+
+(* logic *)
+let int_of_bool = function
+  | true -> 1
+  | false -> 0
+
+let bool_of_int = function
+  | 0 -> false
+  | _ -> true
+
+
+
