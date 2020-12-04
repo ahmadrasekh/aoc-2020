0d986d11506ae681461510b4a81541699b5ab7d4
Ahmad Rasekh <ahmad.rasekh@moixa.com>
2020-12-04 15:35:21 +0000

    d4: again, probably wont scale

---
+++ d4/solution.ml
@@ -0,0 +1,38 @@
+#use "../lib/utils.ml"
+#use "./validators.ml"
+
+let parse_input = read_file
+
+let group_passports ls =
+  let rec aux curr acc = function
+    | hd::tl -> (if hd = "" then aux [] (curr::acc) tl else aux (hd::curr) acc tl)
+    | [] -> (curr::acc) in
+  aux [] [] ls
+
+let mk_passport =  
+  List.map (String.split_on_char ' ') >> 
+  List.flatten >>
+  List.map mk_raw_pair
+
+let mk_raw_pair s = Scanf.sscanf s "%3s:%s" (fun k v -> (k, v))
+
+let validate_a ls =
+  (List.mem_assoc "byr" ls) && (List.mem_assoc "iyr" ls) && 
+  (List.mem_assoc "eyr" ls) && (List.mem_assoc "hgt" ls) && 
+  (List.mem_assoc "hcl" ls) && (List.mem_assoc "ecl" ls) &&
+  (List.mem_assoc "pid" ls)  
+
+  let validate_b ls =
+  (validate_a ls ) && 
+  (validate_byr (List.assoc "byr" ls)) && (validate_iyr (List.assoc "iyr" ls)) &&
+  (validate_eyr (List.assoc "eyr" ls)) && (validate_hgt (List.assoc "hgt" ls)) &&
+  (validate_hcl (List.assoc "hcl" ls)) && (validate_ecl (List.assoc "ecl" ls)) &&
+  (validate_pid (List.assoc "pid" ls))
+
+let x =
+  "input" |>
+  parse_input |>
+  group_passports |> 
+  List.map mk_passport |>
+  List.map validate_b |>
+  count_list true
---
+++ d4/validators.ml
@@ -0,0 +1,48 @@
+#use "../lib/utils.ml"
+
+(* byr (Birth Year) - four digits; at least 1920 and at most 2002. *)
+let validate_byr str = try 
+    Scanf.sscanf str "%i" (fun v -> in_range (1920, 2002) v)
+  with Stdlib.Scanf.Scan_failure _ -> false
+
+(* iyr (Issue Year) - four digits; at least 2010 and at most 2020. *)
+let validate_iyr str = try 
+    Scanf.sscanf str "%i" (fun v -> in_range (2010, 2020) v)
+  with Stdlib.Scanf.Scan_failure _ -> false
+
+(* eyr (Expiration Year) - four digits; at least 2020 and at most 2030. *)
+let validate_eyr str = try Scanf.sscanf str "%i" (fun v -> in_range (2020, 2030) v) with Stdlib.Scanf.Scan_failure _ -> false
+
+(* hgt (Height) - a number followed by either cm or in: *)
+(* If cm, the number must be at least 150 and at most 193. *)
+(* If in, the number must be at least 59 and at most 76. *)
+let validate_hgt str = try Scanf.sscanf str "%i%2s" 
+  (fun v m -> 
+      match m with
+        | "cm" -> in_range (150, 193) v
+        | "in" -> in_range (59, 76) v
+        | _ -> false
+  )
+  with Stdlib.Scanf.Scan_failure _ -> false
+
+(* hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f. *)
+let validate_hcl str = try 
+    Scanf.sscanf str "#%[0-9a-f]" (fun v -> (String.length v) = 6)
+  with Stdlib.Scanf.Scan_failure _ -> false
+
+(* ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth. *)
+let validate_ecl str = try Scanf.sscanf str "%s" 
+  (fun v ->  
+      match v with
+        | "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" -> true
+        | _ -> false
+  )
+  with Stdlib.Scanf.Scan_failure _ -> false
+
+(* pid (Passport ID) - a nine-digit number, including leading zeroes. *)
+let validate_pid str = try 
+    Scanf.sscanf str "%[0-9]" (fun v -> (String.length v)=9) 
+  with Stdlib.Scanf.Scan_failure _ -> false
+
+
+(* cid (Country ID) - ignored, missing or not. *)
