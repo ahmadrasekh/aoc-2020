45dbafd93c056d586f80e0edf688d2ea4e3eb24e
Ahmad Rasekh <ahmad.rasekh@moixa.com>
2020-12-04 16:02:09 +0000

    prettier

--- d4/validators.ml
+++ d4/validators.ml
@@ -11,14 +11,16 @@ let validate_iyr str = try
   with Stdlib.Scanf.Scan_failure _ -> false
 
 (* eyr (Expiration Year) - four digits; at least 2020 and at most 2030. *)
-let validate_eyr str = try Scanf.sscanf str "%i" (fun v -> in_range (2020, 2030) v) with Stdlib.Scanf.Scan_failure _ -> false
+let validate_eyr str = try
+    Scanf.sscanf str "%i" (fun v -> in_range (2020, 2030) v)
+  with Stdlib.Scanf.Scan_failure _ -> false
 
 (* hgt (Height) - a number followed by either cm or in: *)
 (* If cm, the number must be at least 150 and at most 193. *)
 (* If in, the number must be at least 59 and at most 76. *)
 let validate_hgt str = try Scanf.sscanf str "%i%2s" 
-  (fun v m -> 
-      match m with
+  (fun v u -> 
+      match u with
         | "cm" -> in_range (150, 193) v
         | "in" -> in_range (59, 76) v
         | _ -> false
@@ -31,18 +33,13 @@ let validate_hcl str = try
   with Stdlib.Scanf.Scan_failure _ -> false
 
 (* ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth. *)
-let validate_ecl str = try Scanf.sscanf str "%s" 
-  (fun v ->  
-      match v with
+let validate_ecl = function
         | "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" -> true
         | _ -> false
-  )
-  with Stdlib.Scanf.Scan_failure _ -> false
 
 (* pid (Passport ID) - a nine-digit number, including leading zeroes. *)
 let validate_pid str = try 
     Scanf.sscanf str "%[0-9]" (fun v -> (String.length v)=9) 
   with Stdlib.Scanf.Scan_failure _ -> false
-
 
 (* cid (Country ID) - ignored, missing or not. *)
