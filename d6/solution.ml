23c8aa16d0eaff750f1ec91ded4d31ddddcee0a7
Ahmad Rasekh <ahmad.rasekh@moixa.com>
2020-12-06 09:02:03 +0000

    prettier

--- d6/solution.ml
+++ d6/solution.ml
@@ -19,7 +19,11 @@ let _ =
   "input" |>
   parse_input |>
   List.map group_to_set |>
-  List.map (fun sets -> List.fold_left (fun s intersect -> ChartSet.inter s intersect) (List.hd sets) (List.tl sets)) |>
+  List.map (fun sets -> 
+    List.fold_left 
+      (fun s intersect -> ChartSet.inter s intersect) 
+      (List.hd sets) 
+      (List.tl sets)
+  ) |>
   List.map ChartSet.cardinal |>
   list_add
-  
